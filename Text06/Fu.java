5e06305929dd96de0ae75444e681a92b42d4dbc0
macong0503 <1512346520@qq.com>
2021-07-21 16:18:04 +0800

    Create Fu.java

---
+++ Text06/Fu.java
@@ -0,0 +1,12 @@
+package com.hqyj.mc.Text06;
+
+public class Fu {
+    String name = "ff";
+    public Fu(){
+        call();
+    }
+
+    public void call(){
+        System.out.println("父类" + name);
+    }
+}
