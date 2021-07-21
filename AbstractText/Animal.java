0e71924a9a1368bd0dc20e8915beb3b5e7a1bff8
macong0503 <1512346520@qq.com>
2021-07-21 16:16:50 +0800

    Create Animal.java

---
+++ AbstractText/Animal.java
@@ -0,0 +1,11 @@
+package com.hqyj.mc.AbstractText;
+
+public abstract class Animal {
+    /*
+    * 1、包含抽象方法的类就是抽象类
+    * 2、抽象类必须要有抽象方法吗?(false)
+    * 3、只能拥有抽象法吗?(false)
+    * 4、抽象方法只能定义在抽象类中
+    * */
+    public abstract void run();
+}
