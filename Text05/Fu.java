d55d06324aee5bdea29db96141e2cc03f3ede325
macong0503 <1512346520@qq.com>
2021-07-21 16:17:55 +0800

    Create Fu.java

---
+++ Text05/Fu.java
@@ -0,0 +1,16 @@
+package com.hqyj.mc.Text05;
+
+public class Fu {
+    //实验权限修饰符
+    void method(){
+        System.out.println("这是一个父类");
+    }
+    //实验返回值范围
+    public Object methodB(){
+        return null;
+    }
+    //试验方法参数和方法名
+    public void methodC(){
+        System.out.println("这是一个父类C");
+    }
+}
