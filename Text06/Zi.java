a9599b09b2660b115765bb907b1c440962119158
macong0503 <1512346520@qq.com>
2021-07-21 16:18:09 +0800

    Create Zi.java

---
+++ Text06/Zi.java
@@ -0,0 +1,15 @@
+package com.hqyj.mc.Text06;
+
+public class Zi extends Fu{
+    String name = "zz";
+
+    public Zi(){
+
+    }
+
+    @Override
+    public void call(){
+
+        System.out.println("子类" + name);
+    }
+}
