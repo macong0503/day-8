64c8d7362cc073ea6479512bc9d0ad071e3cc518
macong0503 <1512346520@qq.com>
2021-07-21 16:17:52 +0800

    Create Zi.java

---
+++ Text04/Zi.java
@@ -0,0 +1,10 @@
+package com.hqyj.mc.Text04;
+
+
+public class Zi extends Fu {
+    @Override
+    public void method(){
+        super.method();//访问父亲方法
+        System.out.println("儿子");
+    }
+}
