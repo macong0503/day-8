544438fec0a13e1a407612bcd83f54c501f1d9fe
macong0503 <1512346520@qq.com>
2021-07-21 16:17:20 +0800

    Create Zi.java

---
+++ Text01/Zi.java
@@ -0,0 +1,9 @@
+package com.hqyj.mc.Text01;
+//使用extends关键词，继承
+public class Zi extends Fu{
+    String genner = "man";
+    public void methodA(){
+
+        System.out.println("这是一个子类");
+    }
+}
